44b8e335c64d7e699b1f20dbe778b2e648c333ce
garysm <gary@sabiasoftware.com>
2022-07-14 16:10:36 -0400

    add redis

--- server/bin/server.dart
+++ server/bin/server.dart
@@ -1,11 +1,18 @@
 import 'dart:io';
 
+import 'package:redis/redis.dart';
 import 'package:server/app.dart';
 import 'package:shelf/shelf_io.dart';
+
+late final RedisConnection connection;
+late final Command command;
 
 Future main(List<String> args) async {
   final Stdout logger = stdout;
   logger.writeln('Starting server...');
+  connection = RedisConnection();
+  command = await connection.connect('localhost', 6379);
+  logger.writeln('Connected to Redis');
   final app = App(logger);
   // Use any available host or container IP (usually `0.0.0.0`).
   final ip = InternetAddress.anyIPv4;
